b082dd1c11da2b0d2f9b2846e1ee0983a8f9dba6
Tomasz Gorka <tomasz@gorka.org.pl>
2017-05-23 22:46:05 +0200

    structure with comments

--- src/main.c
+++ src/main.c
@@ -15,40 +15,75 @@ struct TDrzewo {
     TDrzewo *prawy;
 };
 
-int* statystyki() {
+/**
+ * Zebranie statystyk wystepowania znakow w posortowanej tablicy niepowtarzalnych znakow
+ */
+int* statystyki(FILE *statFile) {
   /* instrukcje */
   return NULL;
 }
 
-TDrzewo* stworzTDrzewo() {
+/**
+ * Stworzenie drzewa wystapien uzywanego pozniej do kompresji/dekompresji na podstawie podanych statystyk
+ */
+TDrzewo* stworzTDrzewo(int* statystyki) {
   /* instrukcje */
   return NULL;
 }
 
-void kompresuj(TDrzewo *tdrzewo) {
+/**
+ * Kompresuj ciag wejsciowy do binarnego wyjsciowego.
+ */
+void kompresuj(TDrzewo *tdrzewo, FILE *in, FILE *out) {
   /* instrukcje */
 }
 
-void dekompresuj(TDrzewo *tdrzewo) {
+/**
+ * Dekompresuj binarny ciag wejsciowy do ciagu wyjsciowego.
+ */
+void dekompresuj(TDrzewo *tdrzewo, FILE *in, FILE *out) {
   /* instrukcje */
 }
 
+/**
+ * Glowne wywolanie aplikacji
+ */
 int main() {
     printf("Hoffman 1.0.0. Algorytm to kompresji/dekompresji.\n");
 
+
     printf("Zbieranie statystyk\n");
-    int *stat = statystyki();
+    FILE *statFile = fopen("test/podstawowy.txt", "r");
+
+    int *stat = statystyki(statFile);
+
+    fclose(statFile);
+
 
     printf("Tworzenie drzewa\n");
-    TDrzewo *tdrzewo = stworzTDrzewo();
+    TDrzewo *tdrzewo = stworzTDrzewo(stat);
+
 
     printf("Rozpoczecie kompresji\n");
-    kompresuj(tdrzewo);
+    FILE *inputCompressionFile = fopen("test/podstawowy.txt", "r");
+    FILE *outputCompressionFile = fopen("podstawowy.bin", "wb");
+
+    kompresuj(tdrzewo, inputCompressionFile, outputCompressionFile);
+
+    fclose(inputCompressionFile);
+    fclose(outputCompressionFile);
+
 
     printf("Rozpoczecie dekompresji\n");
-    dekompresuj(tdrzewo);
+    FILE *inputDecompressionFile = fopen("podstawowy.bin", "rb");
+    FILE *outputDecompressionFile = fopen("podstawowy_out.txt", "w");
+
+    dekompresuj(tdrzewo, inputDecompressionFile, outputDecompressionFile);
+
+    fclose(inputDecompressionFile);
+    fclose(outputDecompressionFile);
+
 
     printf("Zakonczenie dzialania.\n");
-
     return 0;
 }
